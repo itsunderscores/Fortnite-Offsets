5bafc623f67a1dc69eacba882d19fc8b51457ae8
Underscores <84211119+itsunderscores@users.noreply.github.com>
2026-07-25 23:42:17 -0400

    Update VisCheck.h

--- VisCheck.h
+++ VisCheck.h
@@ -1,6 +1,6 @@
 bool IsEnemyVisible(uintptr_t Mesh) {
 	auto Seconds = memory.read<double>(cache::uworld + offsets::core::Seconds);
-	auto LastRenderTime = memory.read<float>(mesh + offsets::core::LastRenderTime);
+	auto LastRenderTime = memory.read<float>(Mesh + offsets::core::LastRenderTime);
 	if (Seconds - LastRenderTime > 0.06f) return false;
 
 	return true;
